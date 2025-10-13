842bc5f94889af60a8ab6eaa6aa4bf7f9815c4e8
sudipeb <sudip.koirala@ebpearls.com>
2025-10-13 14:11:38 +0545

    snowy day dart created(part of onboarding page)

--- lib/presentation/onboarding_screen.dart
+++ lib/presentation/onboarding_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/constants/app_constants.dart';
+import 'package:weather_app/presentation/onboarding/rainy_day.dart';
+import 'package:weather_app/presentation/onboarding/snowy_day.dart';
 import 'package:weather_app/presentation/onboarding/sunny_day.dart';
 
 class OnboardingScreen extends StatefulWidget {
@@ -13,8 +16,20 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.all(20),
-        child: PageView(children: [SunnyDay()]),
+        padding: const EdgeInsets.only(bottom: 20),
+        child: PageView(children: [SunnyDay(), RainyDay(), SnowyDay()]),
+      ),
+      bottomSheet: Container(
+        decoration: BoxDecoration(gradient: ColorConstants.bodyGradient),
+        height: 80,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            TextButton(onPressed: () {}, child: Text('Skip')),
+            TextButton(onPressed: () {}, child: Text('Next')),
+          ],
+        ),
       ),
     );
   }
