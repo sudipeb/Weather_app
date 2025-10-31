4508b90a03502c5a9347287ce604f52a3ba90f08
sudipeb <sudip.koirala@ebpearls.com>
2025-10-31 10:00:37 +0545

    created the weather event

--- lib/presentation/blocs/weather_event.dart
+++ lib/presentation/blocs/weather_event.dart
@@ -1,1 +1,13 @@
+sealed class WeatherEvent {
+  const WeatherEvent();
+}
 
+final class CurrentLocation extends WeatherEvent {
+  const CurrentLocation();
+}
+
+final class LoadLocationBySearching extends WeatherEvent {
+  const LoadLocationBySearching(this.latitude, this.longitude);
+  final double latitude;
+  final double longitude;
+}
