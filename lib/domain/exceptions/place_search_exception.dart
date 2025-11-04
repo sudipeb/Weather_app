f52ba633c04f44b7010b16eddd7bcaf0f6f99425
sudipeb <sudip.koirala@ebpearls.com>
2025-11-04 12:08:21 +0545

    commenting structure changed

--- lib/domain/exceptions/place_search_exception.dart
+++ lib/domain/exceptions/place_search_exception.dart
@@ -1,5 +1,7 @@
 class PlaceServiceException implements Exception {
   final String message;
+
+  /// Creates a new [PlaceServiceException] with the given [message].
   const PlaceServiceException(this.message);
 
   @override
