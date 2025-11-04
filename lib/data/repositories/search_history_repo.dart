bb046f983c40abfcfceb36054b0a9169eb9b5322
sudipeb <sudip.koirala@ebpearls.com>
2025-11-04 10:13:26 +0545

    minor changes on chekcing the condition and await implementation

--- lib/data/repositories/search_history_repo.dart
+++ lib/data/repositories/search_history_repo.dart
@@ -1,23 +1,30 @@
 import 'package:hive_flutter/hive_flutter.dart';
+import '../../domain/entity/searchbar/place_details.dart';
+import '../../domain/repositories/i_search_history_repository.dart';
 
-import 'package:weather_app/domain/entity/searchbar/place_details.dart';
-
-/// Repository responsible for managing search history using Hive local storage.
-class SearchHistoryRepository {
-  ///Hive box that stores all [Place] objects.
+/// Hive implementation of the search history repository.
+class SearchHistoryRepository implements ISearchHistoryRepository {
   final Box<Place> _box;
+
   SearchHistoryRepository(this._box);
+
+  @override
   Future<void> addSearch(Place search) async {
-    final existingLocation = _box.values.any(
+    final exists = _box.values.any(
       (item) => item.name.toLowerCase() == search.name.toLowerCase(),
     );
-    if (!existingLocation) {
+    if (!exists) {
       await _box.add(search);
     }
   }
 
+  @override
   List<Place> getHistory() => _box.values.toList().reversed.toList();
-  Future<void> clearHistory() async => _box.clear();
+
+  @override
+  Future<void> clearHistory() async => await _box.clear();
+
+  @override
   Future<void> deleteItem(String name) async {
     for (var key in _box.keys) {
       final item = _box.get(key);
