b045483bb811a0766358b88ba8021ff04bece388
sudipeb <sudip.koirala@ebpearls.com>
2025-11-04 10:04:36 +0545

    better error handling implemented

--- lib/service/fetch_places.dart
+++ lib/service/fetch_places.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:weather_app/core/constants/api_constants.dart';
 import 'package:weather_app/data/models/place_model.dart';
 import 'package:weather_app/domain/entity/searchbar/place_details.dart';
+import 'package:weather_app/domain/exceptions/place_search_exception.dart';
 
 Future<List<Place>> fetchPlaceSuggestions(String query) async {
   if (query.isEmpty) return [];
@@ -15,12 +15,29 @@ Future<List<Place>> fetchPlaceSuggestions(String query) async {
         headers: {'X-Api-Key': ApiConstants.ninjaapikey}, //your API key
       ),
     );
+    if (response.data is! List) {
+      throw PlaceServiceException("Unexpected API response format");
+    }
 
     return (response.data as List)
         .map((json) => PlaceModel.fromJson(json as Map<String, dynamic>))
         .toList();
-  } catch (e) {
-    debugPrint('Error fetching city info: $e');
-    return [];
+  } on DioException catch (e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        throw PlaceServiceException("Connection timeout. Please try again.");
+      case DioExceptionType.receiveTimeout:
+        throw PlaceServiceException("Receive timeout. Please try again.");
+      case DioExceptionType.sendTimeout:
+        throw PlaceServiceException("Send timeout. Please try again.");
+      case DioExceptionType.badResponse:
+        throw PlaceServiceException("Server error: ${e.response?.statusCode}");
+      case DioExceptionType.cancel:
+        throw PlaceServiceException("Request was cancelled.");
+      case DioExceptionType.connectionError:
+        throw PlaceServiceException("No internet connection.");
+      default:
+        throw PlaceServiceException("Unexpected network error: ${e.message}");
+    }
   }
 }
