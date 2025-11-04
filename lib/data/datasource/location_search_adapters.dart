ba25552cab284d103cf49eeb5e995e5351e97964
sudipeb <sudip.koirala@ebpearls.com>
2025-11-04 11:47:18 +0545

    added the comments

--- lib/data/datasource/location_search_adapters.dart
+++ lib/data/datasource/location_search_adapters.dart
@@ -1,10 +1,19 @@
 import 'package:hive/hive.dart';
 import 'package:weather_app/domain/entity/searchbar/place_details.dart';
 
+/// Hive [TypeAdapter] for the [Place] entity.
+///
+/// This adapter allows [Place] objects to be stored and
+/// retrieved from a Hive box.
 class LocationSearchAdapter extends TypeAdapter<Place> {
+  /// Unique type ID for Hive. Must be unique across all adapters.
   @override
   final int typeId = 0;
 
+  /// Reads a [Place] object from the binary [reader].
+  ///
+  /// Hive stores data as bytes, so this method converts
+  /// the bytes back into a [Place] object.
   @override
   Place read(BinaryReader reader) {
     final numOffFields = reader.readByte();
@@ -18,6 +27,9 @@ class LocationSearchAdapter extends TypeAdapter<Place> {
     );
   }
 
+  /// Writes a [Place] object to the binary [writer].
+  ///
+  /// Hive will persist the data as bytes for fast storage and retrieval.
   @override
   void write(BinaryWriter writer, Place obj) {
     writer
