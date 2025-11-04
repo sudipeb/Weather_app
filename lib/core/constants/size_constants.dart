fed0c14e81cad9dcce3c3541a3868f9008fd459f
sudipeb <sudip.koirala@ebpearls.com>
2025-11-04 12:32:26 +0545

    size constants setup

---
+++ lib/core/constants/size_constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SizeConstants {
+  static const double heading1 = 34; // e.g., HomeScreen temperature
+  static const double heading2 = 28; // e.g., Location name
+  static const double heading3 = 24; // Card headings
+  static const double bodyLarge = 20; // descriptive text, UV/Wind
+  static const double bodyMedium = 16; // subtitles
+  static const double bodySmall = 14; // smaller info, footer
+  static const double bodyXS = 12; // very small labels
+
+  // -------------------
+  // ICON SIZES
+  // -------------------
+  static const double iconSmall = 16;
+  static const double iconMedium = 24;
+  static const double iconLarge = 32;
+}
