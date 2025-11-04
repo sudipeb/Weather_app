1784db95dd0f3ccb8aab35979b11474336e50dba
sudipeb <sudip.koirala@ebpearls.com>
2025-11-04 11:58:52 +0545

    improved the commenting structure

--- lib/data/models/place_model.dart
+++ lib/data/models/place_model.dart
@@ -4,6 +4,7 @@ import 'package:weather_app/domain/entity/searchbar/place_details.dart';
 part 'place_model.freezed.dart';
 part 'place_model.g.dart';
 
+/// Data model representing a [Place], used for API responses and local storage.
 @freezed
 abstract class PlaceModel with _$PlaceModel implements Place {
   const factory PlaceModel({
@@ -11,6 +12,10 @@ abstract class PlaceModel with _$PlaceModel implements Place {
     required double latitude,
     required double longitude,
   }) = _PlaceModel;
+
+  /// Factory constructor to create a [PlaceModel] from a JSON map.
+  ///
+  /// Delegates to the generated `_$PlaceModelFromJson` method.
   factory PlaceModel.fromJson(Map<String, dynamic> json) =>
       _$PlaceModelFromJson(json);
 }
